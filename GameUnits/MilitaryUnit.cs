07c48bc2cdb06365cd320c5cecc128337487a2fb
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-04 11:55:54 +0100

    Criei class MilitaryUnit e atualizei-a

---
+++ GameUnits/MilitaryUnit.cs
@@ -0,0 +1,32 @@
+namespace GameUnits;
+
+public class MilitaryUnit : Unit
+{
+    private int AttackPower { get; }
+    private int Xp { get; set; }
+    public override float Cost { get; }
+
+    public MilitaryUnit(int movement, int health, int attackPower) : base(movement, health)
+    {
+        AttackPower = attackPower;
+        Xp = 0;
+        Health += Xp;
+        Cost = AttackPower + Xp;
+    }
+
+    public void Attack(Unit u)
+    {
+        if (u.Health == 0) return;
+        else
+        {
+            u.Health -= AttackPower;
+            if (u.Health < 0) u.Health = 0;
+            
+            Xp++;
+        }
+    }
+    public override string ToString()
+    {
+        return $"Military Unit: HP = {Health} Cost = {Cost} Attack Power = {AttackPower} XP = {Xp}";
+    }
+}
