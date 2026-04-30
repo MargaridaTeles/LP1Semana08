7fe53f96a69d6234c92fa82c09389e4c4230a75b
MargaridaTeles <telesmaegarida@gmail.com>
2026-04-30 10:45:30 +0100

    Update class Unit

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -1,14 +1,40 @@
+using System;
+
 namespace GameUnit
 {
-    public class Unit
+    public abstract class Unit
     {
         private readonly int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
+        public abstract float Costa { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"Moved {ToRoman(movement)} spaces.");
+        }
+
+        private string ToRoman(int number)
+        {
+            return number switch
+            {
+                1 => "I",
+                2 => "II",
+                3 => "III",
+                4 => "IV",
+                5 => "V",
+                6 => "VI",
+                7 => "VII",
+                8 => "VIII",
+                9 => "IX",
+                10 => "X",
+                _ => number.ToString()
+            };
         }
     }
 }
